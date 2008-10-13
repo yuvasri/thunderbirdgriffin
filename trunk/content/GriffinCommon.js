c915698549691537cb26b0f0c48fe08dcdfac0e1
monkeypushbutton <monkeypushbutton@ed8bac06-93a2-11dd-996a-531a473a38da>
2008-10-13 16:58:38 +0000

    Add message content to salesforce.

--- trunk/content/GriffinCommon.js
+++ trunk/content/GriffinCommon.js
@@ -1,4 +1,33 @@
 ﻿var GriffinCommon = {
     addMessageToSalesforce: function(){
+    },
+        
+    getPrefValue: function(pref, type) {
+        // Get the "extensions.griffin." branch
+        var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+        prefs = prefs.getBranch("extensions.griffin.");
+        switch (type.toLowerCase()){
+            case "string": return prefs.getCharPref(pref);
+            case "int": return prefs.getIntPref(pref);
+            case "bool": return prefs.getBoolPref(pref);
+            case "datetime": return new Date(pref.getCharPref(pref));
+            case "float": return prefs.getCharPref(pref);
+            default: return prefs.getCharPref(pref);
+        }
+    },
+    
+    setPrefValue: function(pref, value, type){        
+        // Get the "extensions.griffin." branch
+        var prefs = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+        prefs = prefs.getBranch("extensions.griffin.");
+        switch (type.toLowerCase()){
+            case "string": prefs.setCharPref(pref, value);
+            case "int": prefs.setIntPref(pref, value);
+            case "bool": prefs.setBoolPref(pref, value);
+            case "datetime": pref.setCharPref(pref, value);
+            case "float": prefs.setCharPref(pref, value);
+            default: prefs.setCharPref(pref, value);
+        }
     }
+
 };
--- trunk/content/overlay.js
+++ trunk/content/overlay.js
@@ -14,14 +14,33 @@
         }
         // TODO: Find out where GetSelectedMessages is defined??
         var messages = GetSelectedMessages();
+
         var tasks = [];
         for(var i = 0; i < messages.length; i++){
+            var content = "";            
+            var MsgService = messenger.messageServiceFromURI(MessageURI);
+            var MsgStream =  Components.classes["@mozilla.org/network/sync-stream-listener;1"].createInstance();
+            var consumer = MsgStream.QueryInterface(Components.interfaces.nsIInputStream);
+            var ScriptInput = Components.classes["@mozilla.org/scriptableinputstream;1"].createInstance();
+            var ScriptInputStream = ScriptInput.QueryInterface(Components.interfaces.nsIScriptableInputStream);
+            ScriptInputStream.init(consumer);
+            try {
+              MsgService.streamMessage(MessageURI, MsgStream, msgWindow, null, false, null);
+            } catch (ex) {
+              alert("error: "+ex)
+            }
+            ScriptInputStream .available();
+            while (ScriptInputStream .available()) {
+              content = content + ScriptInputStream .read(512);
+            }
             var task = new sforce.SObject('Task');
             var hdr = messenger.msgHdrFromURI(messages[i]);
+            task.Description = content;
             task.Subject = hdr.subject;
             tasks.push(task);
         }
         sforce.connection.create(tasks);
+
     },
     
     openOptions: function(e){
