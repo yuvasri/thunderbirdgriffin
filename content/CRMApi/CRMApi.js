c9f118d9ade0a78db0f52338ba0803ff2552dfd9
monkeypushbutton <monkeypushbutton@ed8bac06-93a2-11dd-996a-531a473a38da>
2008-11-24 16:11:11 +0000

    Unstable checkin - Move to Home Pc
    Continuing move towards support for other CRMs

--- content/CRMApi/CRMApi.js
+++ content/CRMApi/CRMApi.js
@@ -17,271 +17,165 @@ Griffin.SupportedCRMs = {};
 Base CRM Client
 */
 
-Griffin.Crm = function(wsdl){
-    this.wsdl = wsdl;
+Griffin.Crm = function(ns){
+    this.ns = ns;
     this.endpoint = GriffinCommon.getPrefValue("serverUrl", "string");
 }
 
-Griffin.Crm.prototype.invoke = function(method, arrParams, callback){
+Griffin.Crm.prototype.invoke = function(method, params, callback){
     var asynch = callback && callback != null;
-    SOAPClient.invoke(this.wsdl, this.endpoint, method, arrParams, asynch, callback);
+    var retVal = SOAPClient.invoke(this.endpoint, method, params, asynch, callback, this.ns);
+    if(!asynch){
+        GriffinCommon.log(retVal, true);
+    }
+    return retVal;
 };
 
 /*
 Salesforce client
 */
 
-Griffin.SupportedCRMs.Salesforce = new Griffin.Crm("chrome:\\griffin\content\CRMApi\salesforce.wsdl");
+Griffin.SupportedCRMs.Salesforce = new Griffin.Crm("urn:partner.soap.sforce.com");
+Griffin.SupportedCRMs.Salesforce.login = function (username, password){
+    var params = new SOAPClientParameters();
+    params.add("password", password);
+    params.add("username", username);
+    this.invoke("login", params);
+    this.sessionId = result.sessionId;
+    this.serverUrl = result.serverUrl;
+}
 Griffin.SupportedCRMs.Salesforce.insert = function(sobjects){
     this.invoke("insert", sobjects);
 };
 
-// soapclient.js
-
-/*****************************************************************************\
-
- Javascript "SOAP Client" library
-
- @version: 1.4 - 2005.12.10
- @author: Matteo Casati, Ihar Voitka - http://www.guru4.net/
- @description: (1) SOAPClientParameters.add() method returns 'this' pointer.
-               (2) "_getElementsByTagName" method added for xpath queries.
-               (3) "_getXmlHttpPrefix" refactored to "_getXmlHttpProgID" (full 
-                   ActiveX ProgID).
-               
- @version: 1.3 - 2005.12.06
- @author: Matteo Casati - http://www.guru4.net/
- @description: callback function now receives (as second - optional - parameter) 
-               the SOAP response too. Thanks to Ihar Voitka.
-               
- @version: 1.2 - 2005.12.02
- @author: Matteo Casati - http://www.guru4.net/
- @description: (1) fixed update in v. 1.1 for no string params.
-               (2) the "_loadWsdl" method has been updated to fix a bug when 
-               the wsdl is cached and the call is sync. Thanks to Linh Hoang.
-               
- @version: 1.1 - 2005.11.11
- @author: Matteo Casati - http://www.guru4.net/
- @description: the SOAPClientParameters.toXML method has been updated to allow
-               special characters ("<", ">" and "&"). Thanks to Linh Hoang.
-
- @version: 1.0 - 2005.09.08
- @author: Matteo Casati - http://www.guru4.net/
- @notes: first release.
-
-\*****************************************************************************/
-
 function SOAPClientParameters()
 {
 	var _pl = new Array();
+	
 	this.add = function(name, value) 
 	{
 		_pl[name] = value; 
 		return this; 
+	};
+	
+	this._toXml = function(name, value){
+	    var xml = "<" + name + ">";
+	    if(typeof(value) == "SOAPClientParameters"){
+	        xml += value.toXml();
+	    }
+	    else{
+	        xml += value.toString().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+	    }
+	    return xml + "</" + name + ">";
 	}
+	
 	this.toXml = function()
 	{
 		var xml = "";
 		for(var p in _pl)
 		{
-			if(typeof(_pl[p]) != "function")
-				xml += "<" + p + ">" + _pl[p].toString().replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + "</" + p + ">";
+			if(typeof(_pl[p]) == "Array"){
+			    for(var i = 0; i < _pl[p].length; i++)
+			        xml += this._toXml(p, _pl[p][i]);
+			}
+			else if(typeof(_pl[p]) != "function")
+				xml += this._toXml(p, _pl[p]);
 		}
 		return xml;	
-	}
+	};
+	
 }
 
-function SOAPClient() {}
+var SOAPClient = {
+    invoke: function(url, method, parameters, async, callback, ns)
+    {
+	    // build SOAP request
+	    var sr = 
+				    "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+				    "<soap:Envelope " +
+				    "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+				    "xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
+				    "xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
+				    "<soap:Body>" +
+				    "<" + method + " xmlns=\"" + ns + "\">" +
+				    parameters.toXml() +
+				    "</" + method + "></soap:Body></soap:Envelope>";
+	    GriffinCommon.log(sr, true);
+	    // send request
+	    var xmlHttp = new XMLHttpRequest();
+	    xmlHttp.open("POST", url, async);
+	    var soapaction = ((ns.lastIndexOf("/") != ns.length - 1) ? ns + "/" : ns) + method;
+	    xmlHttp.setRequestHeader("SOAPAction", soapaction);
+	    xmlHttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
+	    if(async) 
+	    {
+		    xmlHttp.onreadystatechange = function() 
+		    {
+			    if(xmlHttp.readyState == 4)
+				    SOAPClient.parseResult(xmlHttp, method, callback );
+		    }
+	    }
+	    xmlHttp.send(sr);
+	    if (!async)
+		    return SOAPClient.parseResult(xmlHttp, method, callback );
+    },
+   
+   /* Typical responses:
+   SFDC Login err: 
+<?xml version="1.0" encoding="UTF-8"?>
+<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">
+ <soapenv:Body>
+  <soapenv:Fault>
+   <faultcode xmlns:ns1="urn:fault.partner.soap.sforce.com">ns1:INVALID_LOGIN</faultcode>
+   <faultstring>INVALID_LOGIN: Invalid username, password, security token; or user locked out.</faultstring>
+   <detail>
+    <sf:fault xsi:type="sf:LoginFault" xmlns:sf="urn:fault.partner.soap.sforce.com">
+     <sf:exceptionCode>INVALID_LOGIN</sf:exceptionCode>
+     <sf:exceptionMessage>Invalid username, password, security token; or user locked out.</sf:exceptionMessage>
+    </sf:fault>
+   </detail>
+  </soapenv:Fault>
+ </soapenv:Body>
+</soapenv:Envelope>
 
-SOAPClient.invoke = function(wsdlUrl, url, method, parameters, async, callback)
-{
-	if(async)
-		SOAPClient._loadWsdl(wsdlUrl, url, method, parameters, async, callback);
-	else
-		return SOAPClient._loadWsdl(wsdlUrl, url, method, parameters, async, callback);
-}
-
-// private: wsdl cache
-SOAPClient_cacheWsdl = new Array();
-
-// private: invoke async
-SOAPClient._loadWsdl = function(wsdlUrl, url, method, parameters, async, callback)
-{
-	// load from cache?
-	var wsdl = SOAPClient_cacheWsdl[wsdlUrl];
-	if(wsdl + "" != "" && wsdl + "" != "undefined")
-		return SOAPClient._sendSoapRequest(url, method, parameters, async, callback, wsdl);
-	// get wsdl
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", wsdlUrl, async);
-	if(async) 
-	{
-		xmlHttp.onreadystatechange = function() 
-		{
-			if(xmlHttp.readyState == 4)
-				SOAPClient._onLoadWsdl(url, method, parameters, async, callback, xmlHttp);
-		}
-	}
-	xmlHttp.send(null);
-	if (!async)
-		return SOAPClient._onLoadWsdl(url, method, parameters, async, callback, xmlHttp);
-}
-SOAPClient._onLoadWsdl = function(url, method, parameters, async, callback, req)
-{
-	var wsdl = req.responseXML;
-	SOAPClient_cacheWsdl[url] = wsdl;	// save a copy in cache
-	return SOAPClient._sendSoapRequest(url, method, parameters, async, callback, wsdl);
-}
-SOAPClient._sendSoapRequest = function(url, method, parameters, async, callback, wsdl)
-{
-	// get namespace
-	var ns = (wsdl.documentElement.attributes["targetNamespace"] + "" == "undefined") ? wsdl.documentElement.attributes.getNamedItem("targetNamespace").nodeValue : wsdl.documentElement.attributes["targetNamespace"].value;
-	// build SOAP request
-	var sr = 
-				"<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-				"<soap:Envelope " +
-				"xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
-				"xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" " +
-				"xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">" +
-				"<soap:Body>" +
-				"<" + method + " xmlns=\"" + ns + "\">" +
-				parameters.toXml() +
-				"</" + method + "></soap:Body></soap:Envelope>";
-	// send request
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("POST", url, async);
-	var soapaction = ((ns.lastIndexOf("/") != ns.length - 1) ? ns + "/" : ns) + method;
-	xmlHttp.setRequestHeader("SOAPAction", soapaction);
-	xmlHttp.setRequestHeader("Content-Type", "text/xml; charset=utf-8");
-	if(async) 
-	{
-		xmlHttp.onreadystatechange = function() 
-		{
-			if(xmlHttp.readyState == 4)
-				SOAPClient._onSendSoapRequest(method, async, callback, wsdl, xmlHttp);
-		}
-	}
-	xmlHttp.send(sr);
-	if (!async)
-		return SOAPClient._onSendSoapRequest(method, async, callback, wsdl, xmlHttp);
-}
-SOAPClient._onSendSoapRequest = function(method, async, callback, wsdl, req)
-{
-	var o = null;
-	var nd = SOAPClient._getElementsByTagName(req.responseXML, method + "Result");
-	if(nd.length == 0)
-	{
-		if(req.responseXML.getElementsByTagName("faultcode").length > 0)
-			throw new Error(500, req.responseXML.getElementsByTagName("faultstring")[0].childNodes[0].nodeValue);
-	}
-	else
-		o = SOAPClient._soapresult2object(nd[0], wsdl);
-	if(callback)
-		callback(o, req.responseXML);
-	if(!async)
-		return o;		
-}
-
-// private: utils
-SOAPClient._getElementsByTagName = function(document, tagName)
-{
-	try
-	{
-		// trying to get node omitting any namespaces (latest versions of MSXML.XMLDocument)
-		return document.selectNodes(".//*[local-name()=\""+ tagName +"\"]");
-	}
-	catch (ex) {}
-	// old XML parser support
-	return document.getElementsByTagName(tagName);
-}
-
-SOAPClient._soapresult2object = function(node, wsdl)
-{
-	return SOAPClient._node2object(node, wsdl);
-}
-SOAPClient._node2object = function(node, wsdl)
-{
-	// null node
-	if(node == null)
-		return null;
-	// text node
-	if(node.nodeType == 3 || node.nodeType == 4)
-		return SOAPClient._extractValue(node, wsdl);
-	// leaf node
-	if (node.childNodes.length == 1 && (node.childNodes[0].nodeType == 3 || node.childNodes[0].nodeType == 4))
-		return SOAPClient._node2object(node.childNodes[0], wsdl);
-	var isarray = SOAPClient._getTypeFromWsdl(node.nodeName, wsdl).toLowerCase().indexOf("arrayof") != -1;
-	// object node
-	if(!isarray)
-	{
-		var obj = null;
-		if(node.hasChildNodes())
-			obj = new Object();
-		for(var i = 0; i < node.childNodes.length; i++)
-		{
-			var p = SOAPClient._node2object(node.childNodes[i], wsdl);
-			obj[node.childNodes[i].nodeName] = p;
-		}
-		return obj;
-	}
-	// list node
-	else
-	{
-		// create node ref
-		var l = new Array();
-		for(var i = 0; i < node.childNodes.length; i++)
-			l[l.length] = SOAPClient._node2object(node.childNodes[i], wsdl);
-		return l;
-	}
-	return null;
-}
-SOAPClient._extractValue = function(node, wsdl)
-{
-	var value = node.nodeValue;
-	switch(SOAPClient._getTypeFromWsdl(node.parentNode.nodeName, wsdl).toLowerCase())
-	{
-		default:
-		case "s:string":			
-			return (value != null) ? value + "" : "";
-		case "s:boolean":
-			return value+"" == "true";
-		case "s:int":
-		case "s:long":
-			return (value != null) ? parseInt(value + "", 10) : 0;
-		case "s:double":
-			return (value != null) ? parseFloat(value + "") : 0;
-		case "s:datetime":
-			if(value == null)
-				return null;
-			else
-			{
-				value = value + "";
-				value = value.substring(0, value.lastIndexOf("."));
-				value = value.replace(/T/gi," ");
-				value = value.replace(/-/gi,"/");
-				var d = new Date();
-				d.setTime(Date.parse(value));										
-				return d;				
-			}
-	}
-}
-SOAPClient._getTypeFromWsdl = function(elementname, wsdl)
-{
-	var ell = wsdl.getElementsByTagName("s:element");	// IE
-	if(ell.length == 0)
-		ell = wsdl.getElementsByTagName("element");	// MOZ
-	for(var i = 0; i < ell.length; i++)
-	{
-		if(ell[i].attributes["name"] + "" == "undefined")	// IE
-		{
-			if(ell[i].attributes.getNamedItem("name") != null && ell[i].attributes.getNamedItem("name").nodeValue == elementname && ell[i].attributes.getNamedItem("type") != null) 
-				return ell[i].attributes.getNamedItem("type").nodeValue;
-		}	
-		else // MOZ
-		{
-			if(ell[i].attributes["name"] != null && ell[i].attributes["name"].value == elementname && ell[i].attributes["type"] != null)
-				return ell[i].attributes["type"].value;
-		}
-	}
-	return "";
+   */ 
+    parseResult: function(xmlHttp, method, callback){
+        GriffinCommon.log(xmlHttp.responseText, true);
+        var errs = xmlHttp.responseXML.getElementsByTagName("Fault");
+        if(errs.length > 0){
+            throw SOAPClient.DOM2Obj(errs[0]);
+        }
+        else 
+            return SOAPClient.DOM2Obj(xmlHttp.responseXML.getElementsByTagName("Body")[0]);
+    },
+    
+    DOM2Obj: function(node){
+        var obj = {};
+        for(var attIdx = 0; attIdx < node.attributes.length; attIdx++){
+            var currAtt = node.attributes[attIdx];
+            obj[currAtt.nodeName] = currAtt.nodeValue;
+        }
+        for(var childIdx = 0; childIdx < node.childNodes.length; childIdx++){
+            var currChild = node.childNodes[childIdx];
+            if(currChild.nodeType == Node.TEXT_NODE){
+                continue;
+            }
+            if(currChild.childNodes.length == 0){
+                obj[currChild.nodeName] = null;
+            }
+            else if(currChild.childNodes.length == 1 && currChild.attributes.length == 0 && currChild.firstChild.nodeType == Node.TEXT_NODE){
+                obj[currChild.nodeName] = currChild.firstChild.nodeValue;
+            }
+            else{
+                obj[currChild.nodeName] = SOAPClient.DOM2Obj(currChild);
+            }
+        }
+        obj.toString = function(){
+            var string = "";
+            for(prop in obj){
+                string += prop + ": " + obj[prop].toString() + "\r\n";
+            }
+        }
+        return obj;
+    }
 }
